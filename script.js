ee10d8061b3668db812be9725abff7bb4d2bdcb2
Victory <you@example.com>
2026-08-19 17:46:25 +0200

    ..

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Fetch products dynamically from PHP backend
     async function loadProducts() {
       try {
-        const res = await fetch('/swiss/api.php?action=get_products');
+        const res = await fetch('https://coodesrc.com/swiss/api.php?action=get_products');
         state.products = await res.json();
         renderProducts();
       } catch(e) {
